1827573eb9eefe8138cf191e9223c406a0d16b84
Sandy Abasman <abasifrekesandy@gmail.com>
2024-07-08 10:56:50 +0400

    responsiveness

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,20 @@
 function Footer() {
   return (
     <div className="md:w-full md:flex md:flex-row justify-between items-center h-auto border-t-[0.2px] mt-20  border-gray/40 py-2 px-6 ">
-      <div className="w-2/5 h-auto  flex flex-col gap-2 mt-6">
+      <div className=" md:w-2/5 w-full h-auto  flex flex-col gap-2 mt-6">
         <img className="w-2/5" src="./logo.png" />
         <p className="text-wrap text-xl text-black/50 font-thin leading-7 py-3 ">
           Qui consequat cillum exercitation aliqua sit veniam labore deserunt
           esse culpa ea. Exercitation officia qui nostrud proident cillum
           adipisicing.
         </p>
-        <div className="flex flex-row justify-start items-center gap-3">
-          <img className="w-2/5" src="appple logo.png" />
-          <img className="w-2/5" src="./google logo.png" />
+        <div className="flex w-full flex-row justify-start items-center gap-2">
+          <img className="md:w-2/5" src="appple logo.png" />
+          <img className="md:w-2/5" src="./google logo.png" />
         </div>
       </div>
 
-      <div className="flex flex-row gap-10  justify-evenly items-start">
+      <div className="flex sm:mt-10 flex-row flex-wrap  gap-10   md:justify-evenly md:items-start">
         <ul>
           <li className="font-bold">Information</li>
           <li className="text-gray font-normal">Shop Pay</li>
